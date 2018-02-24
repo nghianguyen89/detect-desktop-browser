9cee8df58ef5a630bee78b7602e6c62ed6400e29
Nghia Nguyen <nghianguyen89@users.noreply.github.com>
2018-02-24 11:18:45 +0700

    upload files
    Upload content

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # detect-desktop-browser
+
+Plugin Name  : CSS Browser Detector
+Description  : Detect Desktop Browser
+Author       : Nghia Nguyen
+Contact      : fb.com/nghianguyen1989
+
+
+
+HISTORY :
+Version 1.0 (Jan 2017) : 
+- detect Microsoft Edge 
+- detect IE browser : IE7, IE8, IE9, IE10, IE11
+- detect Safari browser
+- detect Chrome browser
+- detect Firefox browser
+- detect Opera browser
+- detect browser engine (webkit/gecko)
+
+HOW TO USE : 
+1. Add script
+   <script type="text/javascript" src="detectbrowser.min.js" charset="UTF-8"></script>
+
+2. Add prefix before class/id name CSS
+    Internet Explorer
+    |__IE7       :   .ie7 cssname{...}
+    |__IE8       :   .ie8 cssname{...}
+    |__IE9       :   .ie9 cssname{...}
+    |__IE10      :   .ie10 cssname{...}
+    |__IE11      :   .ie11 cssname{...}
+    |__All IE    :   .ie cssname{...}
+
+    Microsoft Edge
+    |__.edge cssname{...}
+
+    Safari
+    |__.safari cssname{...}
+
+    Google Chrome
+    |__.chrome cssname{...}
+
+    Mozilla Firefox
+    |__.firefox cssname{...}
+
+    Opera
+    |__.opera cssname{...}
+
+    Browser Engine
+    |__.gecko cssname{...}
+    |__.webkit cssname{...}
---
+++ detectbrowser.js
@@ -0,0 +1,33 @@
+﻿/***
+ * Plugin Name  : CSS Browser Detector
+ * Description  : Detect Desktop Browser
+ * Author       : Nghia Nguyen
+ * Contact      : fb.com/nghianguyen1989
+ * Version      : 1.0 (Jan 2017)
+ ***/
+
+function detect_browser(u) {
+    var ua = u.toLowerCase(),
+        isIE11 = !!navigator.userAgent.match(/Trident.*rv\:11\./),
+        isEdge = !!navigator.userAgent.match(/Edge/),
+        isOpera = !!navigator.userAgent.match(/OPR/),
+        is = function (t) {
+            return ua.indexOf(t) > -1
+        },
+        g = 'gecko',
+        w = 'webkit',
+        s = 'safari',
+        arrClass = [
+            (!(/opera|webtv/i.test(ua)) && /msie\s(\d)/.test(ua)) ? ('ie ie' + (!(RegExp.$1 == 1) ? RegExp.$1 : 10)) :
+            isEdge ? (' edge') :
+            is('gecko/') ? g + ' firefox' :
+            isOpera ? ' opera' :
+            is('chrome') ? w + ' chrome' :
+            is('applewebkit/') ? w + ' ' + s + (/version\/(\d+)/.test(ua) ? ' ' + s + RegExp.$1 : '') :
+            is('mozilla/') ? (isIE11 ? ('ie ie11') : g) : ''
+        ];
+    c = arrClass.join(' ');
+    document.documentElement.className += ' ' + c;
+    return c;
+};
+detect_browser(navigator.userAgent);
